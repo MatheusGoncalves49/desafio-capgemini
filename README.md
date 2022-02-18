fb070ba9720b032d469c95359d15eed03904e9d9
Matheus Gonçalves <matheus_mhsg@outlook.com>
2022-02-17 23:34:25 -0300

    insercao de readme.md e ajuste na execucao de questoes

---
+++ README.md
@@ -0,0 +1,16 @@
+#Desafio Capgemini
+
+#### O projeto se refere a um desafio de programação clasificatório para a seleção da empresa Capgemini 
+
+
+##Tecnologias utilizadas
+#### Java 8 (linguagem de programação)
+#### JUnit 5 (framework para criação de testes unitários)
+#### Maven (gerenciador de pacotes / dependdências)
+
+##Instruções de execução
+#### 1 - Clonar o repositório
+#### 2 - Instalar as dependências do maven (comando "mvn install")
+#### 3 - Executar a aplicação como aplicação java (classe src/main/java/Main.java)
+ - A aplicação executará as 3 questões em sequência)
+ - Também é posível executar os testes unitários, presentes na pasta src/test/java 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         executaQuestao1 ();
-//        executaQuestao2 ();
-//        executaQuestao3 ();
+        executaQuestao2 ();
+        executaQuestao3 ();
     }
 
     public static void executaQuestao1 () {
-        System.out.println("Executando questão 1:");
+        System.out.println("Executando questão 1 (escada de *):");
 
         Scanner scanner = new Scanner(System.in);
         int qtdCaracteres = 0;
@@ -29,22 +29,25 @@ public class Main {
     }
 
     public static void executaQuestao2 () {
-        System.out.println("Executando questão 2:");
+        System.out.println("Executando questão 2 (força da senha):");
 
-        Questao2 questao2 = new Questao2();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Informe a senha: ");
         String senha = scanner.nextLine();
-        System.out.println(questao2.run(senha));
+        Questao2 questao2 = new Questao2();
+        System.out.println("é necessário pelo menos mais " + questao2.run(senha) + " caracteres");
 
         System.out.println("Fim execução questão 2\n");
     }
 
     public static void executaQuestao3 () {
-        System.out.println("Executando questão 3:");
+        System.out.println("Executando questão 3 (anagramas):");
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe a palavra: ");
+        String palavra = scanner.nextLine();
         Questao3 questao = new Questao3();
-        System.out.println(questao.run("ovo"));
+        System.out.println("nesta palavra há " + questao.run(palavra) + " anagramas");
 
         System.out.println("Fim execução questão 3\n");
     }
